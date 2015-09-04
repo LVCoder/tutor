177205ca2ef7836a4a28471c5ba2e3c7cfc6c92c
Oleh <Pylypchak@ukr.net>
2015-09-05 01:49:03 +0300

    fixed view bugs

--- TutorOnline/src/main/webapp/WEB-INF/views/public/user_home.jsp
+++ TutorOnline/src/main/webapp/WEB-INF/views/public/user_home.jsp
@@ -1,13 +1,13 @@
 <div ng-controller="UserHomeController">
 	<div class="intro">
 		<div class="row">
-			<div class="col-xs-9">Hourly Rate</div>
-			<div class="col-xs-9">feedbacks</div>
-			<div class="col-xs-9">reviews </div>
-			<div class="col-xs-9"><a href="#/edit_profile">Edit</a> </div>
-			<div class="col-xs-9"><a href="#/search">Search</a> </div>
-			<div class="col-xs-9"><a href="#/messaging">Messages</a> </div>
-			<div class="col-md-offset-9"><img width="70%" src="{{user.avatarPath}}"></img></div>
+			<div class="col-xs-9" style="margin-left:50px;">Hourly Rate</div>
+			<div class="col-xs-9" style="margin-left:50px;">feedbacks</div>
+			<div class="col-xs-9" style="margin-left:50px;">reviews </div>
+			<div class="col-xs-9" style="margin-left:50px;"><a href="#/edit_profile">Edit</a> </div>
+			<div class="col-xs-9" style="margin-left:50px;"><a href="#/search">Search</a> </div>
+			<div class="col-xs-9" style="margin-left:50px;"><a href="#/messaging">Messages</a> </div>
+			<div class="col-md-offset-9" ><img width="70%" src="{{user.avatarPath}}"></img></div>
 		</div>
 		<div class="row">
 			<div class="col-md-offset-9">{{user.firstName}}</div>
--- TutorOnline/src/main/webapp/resources/js/editProfileController.js
+++ TutorOnline/src/main/webapp/resources/js/editProfileController.js
@@ -172,45 +172,12 @@ $('#institution').autocomplete({
 	        headers: {'Content-Type': undefined},
 			data: formData
 		}).success(function(result){
-			
+			location.reload();
 		})
 	
 	}
 	
 	
-//	$scope.edit = function() {
-//
-//		var learnSubjectsIds = [];
-//		for (i = 0; i < $scope.learnSelects.length; i++) {
-//			learnSubjectsIds.push($scope.learnSelects[i].selectedSubject.id);
-//		}
-//		var teachSubjectsIdPrice = [];
-//		for (i = 0; i < $scope.teachSelects.length; i++) {
-//			teachSubjectsIdPrice.push({
-//				subjectId : $scope.teachSelects[i].selectedSubject.id,
-//				price : $scope.teachSelects[i].price
-//			});
-//		}
-//		// var user = {
-//		// firstName : $scope.user.firstName,
-//		// lastName : $scope.lastName,
-//		// username : $scope.username,
-//		// wantLearn : $scope.wantLearn,
-//		// wantTeach : $scope.wantTeach,
-//		// learnSubjectsIds : learnSubjectsIds,
-//		// teachSubjectsIdPrice : teachSubjectsIdPrice,
-//		// experience : $scope.experience,
-//		// others : $scope.others
-//		// }
-//		$http({
-//			method : "POST",
-//			url : "/protected/user/edit",
-//			data : $scope.user,
-//		}).success(function(result) {
-//
-//			$location.path = "/user_home";
-//
-//		})
-//	}
+
 
 }
